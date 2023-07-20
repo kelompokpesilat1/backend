df68314240a118c6d5af6800814c9d2ee63a0f94
Dandywahyudin <dandywahyudin19@gmail.com>
2023-07-20 20:44:06 +0700

    update

--- app/routes/index.js
+++ app/routes/index.js
@@ -1,3 +1,6 @@
+/* eslint-disable no-tabs */
+/* eslint-disable no-mixed-spaces-and-tabs */
+/* eslint-disable no-undef */
 const express = require('express');
 
 const router = express.Router();
@@ -8,4 +11,74 @@ router.get('/', (req, res, next) => {
   res.render('index', { title: 'Express' });
 });
 
-module.exports = router;
+// eslint-disable-next-line func-names
+module.exports = function (app) {
+  app.use((req, res, next) => {
+    res.header(
+		  'Access-Control-Allow-Headers',
+      'x-access-token, Origin, Content-Type, Accept',
+    );
+    next();
+  });
+
+  app.post(
+    '/api/auth/signup',
+    [
+      verifySignUpController.checkDuplicateUserNameOrEmail,
+      verifySignUpController.checkRolesExisted,
+    ],
+    verifySignController.signup,
+  );
+
+  app.post('/api/auth/signin', (req, res) => {
+    verifySignController.signIn(req, res);
+  });
+
+  // Status
+  app.get('/api/status', (req, res) => {
+    statusController.list(req, res);
+  });
+  app.get('/api/statususer', [verifyJwtTokenController.verifyToken], (req, res) => {
+    statusController.listStatusUser(req, res);
+  });
+  app.get(
+    '/api/status/:id',
+    [
+      verifyJwtTokenController.verifyToken,
+      verifyJwtTokenController.isAdmin,
+    ],
+    (req, res) => {
+      statusController.getById(req, res);
+    },
+  );
+  app.post(
+    '/api/status',
+    [
+      verifyJwtTokenController.verifyToken,
+      verifyJwtTokenController.isAdmin,
+    ],
+    (req, res) => {
+      statusController.add(req, res);
+    },
+  );
+  app.put(
+    '/api/status/:id',
+    [
+      verifyJwtTokenController.verifyToken,
+      verifyJwtTokenController.isAdmin,
+    ],
+    (req, res) => {
+      statusController.update(req, res);
+    },
+  );
+  app.delete(
+    '/api/status/:id',
+    [
+      verifyJwtTokenController.verifyToken,
+      verifyJwtTokenController.isAdmin,
+    ],
+    (req, res) => {
+      statusController.delete(req, res);
+    },
+  );
+};
